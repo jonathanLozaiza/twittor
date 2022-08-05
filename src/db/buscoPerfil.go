267dc7a4f4cd33bbf02dc6f08f264de321194a55
jonathanLoaiza <jloaiza@bostonprojectllc.com>
2022-08-04 20:34:57 -0500

    updated 04-08-2022

---
+++ src/db/buscoPerfil.go
@@ -0,0 +1,31 @@
+package db
+
+import (
+	"context"
+	"fmt"
+	"time"
+	"twitter/src/models"
+
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson/primitive"
+)
+
+func BuscoPerfil(ID string) (models.Usuario, error) {
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second*15)
+	defer cancel()
+	db := MongoCN.Database("twitter")
+	col := db.Collection("users")
+
+	var perfil models.Usuario
+	objID, _ := primitive.ObjectIDFromHex(ID)
+
+	condition := bson.M{"_id": objID}
+
+	err := col.FindOne(ctx, condition).Decode(&perfil)
+	perfil.Password = ""
+	if err != nil {
+		fmt.Println("Registro no encontrado" + err.Error())
+		return perfil, err
+	}
+	return perfil, nil
+}
